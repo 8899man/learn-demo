103d0d31a3002a87d4ce33762033864402de0b72
af226 <react.dong.yu@gmail.com>
2016-12-11 16:22:07 +0800

    改接口以及增加配置文件

--- chrome_devtool/source/demo2.js
+++ chrome_devtool/source/demo2.js
@@ -4,37 +4,37 @@
 var linksArray = new Array();
 //提示信息
 var info = '';
-
 //数据源链接
 var url = '';
-//存放电话号码的数组
-// var numberArray = new Array();
-
 //递归定时器
 var t_timeout = null;
-
-//计数器
+//已取链接计数器
 var count = 0;
+//每次取链接条数
+var num = 1;
+//请求地址
+var request_url = 'http://10.2.227.118:3000/';
+
 
 //取链接
 var getLinks = function(){
 	var ajaxTimeout = $.ajax({
-		type: "POST",
-		url: "http://120.27.42.144/whatsapp/get/url/",
+		type: 'POST',
+		url: request_url + 'whatsapp/get/url/',
 		data: {
-			"size": 10,
+			"size": num,
 			"status": status
 		},
 		dataType: 'json',
 		timeout: 5000,
 		success: function(data){
-			if(data.code === '0') {
-				linksArray = linksArray.concat(data.urls);	
-				count += 10;					
+			if(data.code === 0) {
+				linksArray = linksArray.concat(data.urls);
+				count += num;
 			} else {
 				info = '链接已取完';
-				$('div.pane.pane-two').append('<span style="color:red;font-weight:bold;">' + info + '</span>');				
-			}		
+				$('div.pane.pane-two').append('<span style="color:red;font-weight:bold;">' + info + '</span>');
+			}
 		},
 		error: function(XMLHttpRequest,textStatus){	
 			info = textStatus + '取链接时服务器错误，请检查服务器';
@@ -53,37 +53,39 @@ var getLinks = function(){
 var getNum = function() {
 	var phone = {
 		phones: [],
-		tag: ''
+		id: 0,
+		name: '',
+		create_user: ''
 	};
-	if(linksArray.length < 2 && count < 10){
+	if(linksArray.length < 2 && count < 1){
 		getLinks();
 	}
 	url = linksArray.shift();
 	if(url == null || url == '' || url === undefined) {
 		clearTimeout(t_timeout);
-		window.location.reload();
+		// window.location.reload();
 		return;
 	}
+	phone.id = url.id;
 	console.log('群链接剩余：' + linksArray.length);
-	$('div.pane.pane-two').append('<a id="mytarget" href="' + url + '" target="_blank">' + url + '</a>');	
-	console.log('本次群链接是：' + $('#mytarget')[0].innerHTML);
+	$('div.pane.pane-two').append('<a id="mytarget" href="' + url.url + '" target="_blank">.' + '</a>');	
 	$('#mytarget').get(0).click();
 
 	var k = setTimeout(function(){
-		phone.tag = $('.popup-container span.metadata-title').html();
+		phone.name = $('.popup-container span.metadata-title').html();
+		phone.create_user = $('.popup-container span.metadata-subtitle').html();
 		$.each($('.popup-container .participants-container .participant-text'),function(){
 			phone.phones.push($(this).get(0).innerHTML);
 		});
 		$('.popup-container .popup-controls .btn-plain:first-child').click();
 		$('#mytarget').remove();
-		console.log('本次群名称是：' + phone.tag + '---号码总数：' + phone.phones.length);
+		console.log('本次群名称是：' + phone.name + '---号码总数：' + phone.phones.length);
 
 		$.ajax({
 			type: 'POST',
 			contentType: 'application/json;charset=utf-8',
-			url: 'http://120.27.42.144/whatsapp/upload/phone/',
+			url: request_url + 'whatsapp/upload/phone/',
 			data: JSON.stringify(phone),
-			// data: '{"phones":[' + numberArray +'],"tag":"' + groupName + '","admin_phone":"' + admin_phone + '","g_url":"' +  url + '"}',
 			dataType: 'json',
 			// timeout: 8000,
 			success: function(){},
